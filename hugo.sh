f89704721da82f15c2c958c87e0df2223b971c37
Simon Garcia <garcias@kenyon.edu>
2018-05-03 18:32:38 +0000

    getting the latest Hugo release from GitHub

---
+++ hugo.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+API_URL=https://api.github.com/repos/gohugoio/hugo/releases
+RELEASE_ID=`curl $API_URL/latest | jq '.id '`
+ASSET_ID=`curl $API_URL/$RELEASE_ID/assets | jq -r '.[] | "\(.id) \(.name)"' | grep "Linux-64bit.deb" | grep -o "^[0-9]* "`
+ASSET_URL=`curl $API_URL/assets/$ASSET_ID | jq -r '.browser_download_url'`
+wget $ASSET_URL
+sudo dpkg -i hugo*.deb
+hugo version
